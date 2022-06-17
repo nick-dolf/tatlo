e1a14cbea55dbbdfd7e2c4a774afaa84e65e1508
nick-dolf <nickdolf@gmail.com>
2022-06-17 15:27:34 -0500

    added slideshow

--- assets/js/slides.js
+++ assets/js/slides.js
@@ -1,28 +1,30 @@
 let slideIndex = 0
-showSlides(slideIndex)
+let slides = document.getElementsByClassName('slide__image');
+let thumbs = document.getElementsByClassName('slide-thumb');
+showSlides(0)
 
 function next() {
   slideIndex += 1
+  if (slideIndex >= slides.length) {
+     slideIndex = 0
+   }
   showSlides(slideIndex)
 }
 
 function back() {
   slideIndex -= 1
+  if (slideIndex < 0) {
+    slideIndex = slides.length - 1
+  }
   showSlides(slideIndex)
 }
 
 function showSlides(n) {
-  let slides = document.getElementsByClassName('slide__image');
-
-  if (slideIndex < 0) {
-    slideIndex = slides.length - 1
-  } else if (slideIndex >= slides.length) {
-    slideIndex = 0
-  }
-
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none"
+    thumbs[i].classList.remove('active')
   }
 
-  slides[slideIndex].style.display = "block"
+  slides[n].style.display = "block"
+  thumbs[n].classList.add('active')
 }
